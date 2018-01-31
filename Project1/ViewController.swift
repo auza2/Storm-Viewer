0dbf54bde290949f9c5b631599fdab52927bb013
Jamie Auza <jamieauza@Jamies-MacBook-Pro.local>
2018-01-31 12:53:16 -0600

    Project 1

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items =  try! fm.contentsOfDirectory(atPath: path)
+        // use try when there is a possibility that an error will be thrown if you call the method
+        // Not to be used for optionals
+        
+        for filename in items{
+            if( filename.hasPrefix("nssl")){
+                pictures.append(filename)
+            }
+        }
+        
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let detailViewController = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController{
+            detailViewController.pictureName = pictures[indexPath.row]
+            navigationController?.pushViewController(detailViewController, animated: true)
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
